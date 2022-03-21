829abc905575a6f6a5ab1cf7908ce07567ea9d4c
박성훈 / Koder <koder0205@gmail.com>
2022-03-22 02:55:42 +0900

    update
    자잘자잘한 수정

--- js/color.js
+++ js/color.js
@@ -1,13 +1,8 @@
 document.getElementById('color-light').addEventListener("click", SetColorLight);
-document.getElementById('color-normal').addEventListener("click", SetColorNormal);
 document.getElementById('color-dark').addEventListener("click", SetColorDark);
 
 function SetColorLight(){
     document.documentElement.setAttribute('color','light');
-}
-
-function SetColorNormal(){
-    document.documentElement.setAttribute('color','normal');
 }
 
 function SetColorDark(){
