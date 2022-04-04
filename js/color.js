715fe989c8b733ef81efc11fd7fecc7c723c0019
박성훈 / Koder <koder0205@gmail.com>
2022-04-04 19:43:02 +0900

    update
    색테마 저장가능하게 하는 기능 추가
    홈페이지용 이미지 여럿 추가

--- js/color.js
+++ js/color.js
@@ -1,10 +1,19 @@
 document.getElementById('color-light').addEventListener("click", SetColorLight);
 document.getElementById('color-dark').addEventListener("click", SetColorDark);
 
+const storedtheme = localStorage.getItem('colortheme');
+
+if(storedtheme !== null){
+    document.documentElement.setAttribute('color', storedtheme);
+}
+
 function SetColorLight(){
     document.documentElement.setAttribute('color','light');
+    localStorage.setItem("colortheme", 'light');
 }
 
 function SetColorDark(){
     document.documentElement.setAttribute('color','dark');
+    localStorage.setItem("colortheme", 'dark');
 }
+
